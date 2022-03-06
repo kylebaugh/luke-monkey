d5e29c9b81005c37326eeb94c7fee726127f95b3
kylebaugh <kylebaugh1@gmail.com>
2022-03-06 13:32:00 -0700

    adsf

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path')
 const cors = require('cors')
 
 
-const {SESSION_SECRET, SERVER_PORT, CONNECTION_STRING, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env;
+const {SESSION_SECRET, SERVER_PORT, DATABASE_URL, CONNECTION_STRING, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env;
 
 const app = express()
 
@@ -27,7 +27,7 @@ app.use(session({
 }))
 
 massive({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: DATABASE_URL,
     ssl:{
         rejectUnauthorized: false
     }
